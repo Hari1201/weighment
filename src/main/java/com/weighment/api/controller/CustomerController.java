5221c7a078d71e688feca80e48978b1e5e96f2a3
harikrishnan_r <harikrishnan_r@CHL118174.photonint.com>
2020-03-21 15:32:17 +0530

    customer controller validation

--- src/main/java/com/weighment/api/controller/CustomerController.java
+++ src/main/java/com/weighment/api/controller/CustomerController.java
@@ -1,6 +1,9 @@
 package com.weighment.api.controller;
 
 import java.math.BigInteger;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weighment.api.model.CustomerDetails;
@@ -27,6 +29,22 @@ public class CustomerController {
 	@PostMapping
 	public JsonResponse save(@RequestBody CustomerDetails customerDetails) {
 		try {
+		      
+			Optional<CustomerDetails> employee = Optional.ofNullable(customerDetails);
+			Optional<String> name = Optional.ofNullable(customerDetails.getCustomerName());
+			Optional<String> phone = Optional.ofNullable(customerDetails.getCustomerPhone());
+
+			if(employee.isEmpty()) {
+				return new JsonResponse(Boolean.FALSE,"Please enter customer details!!!");
+			}else if(name.isEmpty()) {
+				return new JsonResponse(Boolean.FALSE,"Please select customer!!!");
+			} else if (!phone.isEmpty()) {
+				Pattern pattern = Pattern.compile("\\d{10}");
+				Matcher matcher = pattern.matcher(customerDetails.getCustomerPhone());
+				if (!matcher.matches()) {
+					return new JsonResponse(Boolean.FALSE, "Please enter valid mobile number!!!");
+				}
+			}
 		return customerService.save(customerDetails);
 		}catch (Exception e) {
 			return new JsonResponse(e.getMessage(),"Customer Not Saved!!!",Boolean.FALSE);
